74d7e2698d1c6f7ae0c93564796055142ad32a15
Luis <luis_at3nc0@hotmail.com>
2020-11-18 19:09:22 -0600

    firebase listo

--- app/src/main/java/com/servicio/rapivid_19mx/ui/estados/listaEstadosFragment.java
+++ app/src/main/java/com/servicio/rapivid_19mx/ui/estados/listaEstadosFragment.java
@@ -13,60 +13,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.servicio.rapivid_19mx.R;
-import com.servicio.rapivid_19mx.ui.estados.dummy.DummyContent;
 
 /**
  * A fragment representing a list of Items.
  */
 public class listaEstadosFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public listaEstadosFragment() {
-    }
-
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static listaEstadosFragment newInstance(int columnCount) {
-        listaEstadosFragment fragment = new listaEstadosFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_lista_estados_list, container, false);
-
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS));
-        }
+        View view = inflater.inflate(R.layout.fragment_lista_estados, container, false);
         return view;
     }
 }
